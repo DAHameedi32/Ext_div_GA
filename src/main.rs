1b8402a193c4b634b630fd39ca89b14845b1ca53
Daniyal Hameedi <120378347+DAHameedi32@users.noreply.github.com>
2023-09-13 15:32:49 +0100

    Update main.rs
    fixed dimension errors in the direct product and tensor flattening 
    functions

--- src/main.rs
+++ src/main.rs
@@ -413,61 +413,60 @@ pub fn flatten_to_mat(tensor:Vec<Vec<Mat<f64>>>) -> Mat<f64>
    let y_dim_inner = tensor[0][0].ncols();
    let x_dim_inner = tensor[0][0].nrows();
 
-   let fin_dims_y = (y_dim_outer as i64 + y_dim_inner as i64);
-   let fin_dims_x = (x_dim_outer as i64 + x_dim_inner as i64);
+   let fin_dims_y = (y_dim_outer as i64 * y_dim_inner as i64);
+   let fin_dims_x = (x_dim_outer as i64 * x_dim_inner as i64);
    let mut fin_mat: Mat<f64> = Mat::zeros(fin_dims_y as usize, fin_dims_x as usize);
-   for y_out in 0..y_dim_outer{
-        for x_out in 0..x_dim_outer{
-            for y_in in 0..y_dim_inner{
-                for x_in in 0..x_dim_inner{
-                    let current_pos_y = y_out*y_dim_outer + y_in;
-                    let current_pos_x = x_out*x_dim_outer + x_in;
-                    fin_mat.write(current_pos_y, current_pos_x, tensor[y_out][x_out].read(y_in, x_in));
+   for y_index in 0..y_dim_outer as usize
+   {
+        for x_index in 0..x_dim_outer as usize
+        {
+            for i_index in 0..y_dim_inner
+            {
+                for j_index in 0..x_dim_inner
+                {
+                    fin_mat.write(  y_index*y_dim_outer + i_index,
+                                    x_index*x_dim_outer+ j_index,
+                                  tensor[y_index%y_dim_outer][x_index%x_dim_outer].read(i_index, j_index));
                 }
             }
+            
         }
+        
    }
    return fin_mat;
 }
-
 pub fn direct_prod(A: MatRef<f64>, B: MatRef<f64>) -> Mat<f64>
 {
-    //initialise matrix A
+    //fetch data about A
     let A_dims_cols = A.ncols() as i64;
     let A_dims_rows = A.nrows() as i64;
-    let mut A_buf: Mat<f64> = Mat::zeros(A_dims_cols as usize, A_dims_rows as usize);
-    for i in 0..A_dims_rows{
-        for j in 0..A_dims_cols{
-            A_buf.write(i as usize, j as usize, A.read(i as usize, j as usize));
-        }
-    }
     
-    //initialise matrix B
+    //fetch data about B
     let B_dims_cols = B.ncols() as i64;
     let B_dims_rows = B.nrows() as i64;
-    let mut B_buf: Mat<f64> = Mat::zeros(B_dims_cols as usize, B_dims_rows as usize);
-    for i in 0..B_dims_rows{
-        for j in 0..B_dims_cols{
-            B_buf.write(i as usize, j as usize, B.read(i as usize, j as usize));
-        }
-    }
+
     //compute and collect n+1 rank tensor sub matrices in a vector
     let C_cols = (A_dims_cols) as usize;
     let C_rows = (A_dims_rows) as usize;
-    let mut tensor:Vec<Vec<Mat<f64>>> = vec![vec![]; C_cols];
-    for x in 0..C_cols
+    let mut tensor:Vec<Vec<Mat<f64>>> = vec![vec![]; C_rows];
+    for y in 0..C_rows
     {
-        for y in 0..C_rows
+        for x in 0..C_cols
         {
-            //iterate through submatrices
-            for i in 0..A_dims_rows{
-                for j in 0..A_dims_cols{
-                    let aux_mat : Mat<f64> = Mat::with_dims(A_dims_rows as usize, A_dims_cols as usize, |x, y| if x == y { A_buf.read(i as usize, j as usize) } else { 0.0 }); 
-                    let mut targ_mat = Mat::zeros(B_dims_rows as usize, B_dims_cols as usize);
-                    matmul(targ_mat.as_mut(), aux_mat.as_ref(), B_buf.as_ref(), None, 1.0, faer_core::Parallelism::Rayon(0));
-                    tensor[y].push(targ_mat);
+            let mut sub_mat:Mat<f64> = Mat::zeros(B.nrows(), B.ncols());
+            //make an auxillary matrix because the scalar multiplication keeps on acting up and clearly isnt stable
+            let mut aux_mat:Mat<f64> = Mat::zeros(B.nrows(), B.ncols());
+            for i in 0..B_dims_cols{
+                for j in 0..B_dims_rows{
+                    if(i == j)
+                    {
+                        aux_mat.write(i as usize, j as usize, A.read(y, x))
+                    }
                 }
             }
+            matmul(sub_mat.as_mut(), aux_mat.as_ref(), B,None, 1.0f64, faer_core::Parallelism::Rayon(0));
+
+            tensor[y].push(sub_mat);
         }
 
     }
@@ -475,7 +474,6 @@ pub fn direct_prod(A: MatRef<f64>, B: MatRef<f64>) -> Mat<f64>
     return ret_mat; //returns a matrix
 
 }
-
 pub fn wedge_v(u : MatRef<f64>, v : MatRef<f64>) -> Mat<f64> //wedge product for vectors
 { 
     //This is the process for vectors
@@ -603,9 +601,38 @@ pub fn gaussian_elimination(mut matrix: Mat<f64>) -> Mat<f64> {
     return matrix;
 }
 fn main() {
-    //EXAMPLE CODE:
-    //this shows how to create k-forms beginning from one-forms using the non-module specific functions
-    //code might be re-organised at some point
+    /* 
+    //initial functions in space are numerically described as an evolution from initial conditons.
+    //we can define the 1-form as being the vector rep of the function in space:
+    //F(x,y,z,...)dx + G(x,y,z,...)dy+...
+    //is equivalent to F(...)i + G(...)j +...
+    // let dims : usize = 3; //dimensions of our space
+    //give the explicit 1-forms and the initial conditions we can construct the k-forms as necessary
+    //can compute the functions explicitly:
+    /*
+    let F = 9 * x_1 + 5;
+    let G = 6 * x_2 + 3;
+    let H = 3 * x_3 + 2;
+     */
+    
+    //...
+    //define some restrictions on the problem:
+    //important data for numerical construction:
+    /* 
+    let starting_x_1 = 0;
+    let final_x_1 = 5;
+    let step_x_1 = 0.1;
+
+    let starting_x_2 = 0;
+    let final_x_2 = 5;
+    let step_x_2 = 0.1;
+
+    let starting_x_3 = 0;
+    let final_x_3 = 5;
+    let step_x_3 = 0.1;
+    */
+    //constructing the functions:
+    */
     //once we compute the functions in F(), G(), ... we can compute the wedge at all points
     let p = mat!([2.0f64, 0.0f64, 0.0f64, 0.0f64]); //these row vectors will contain the output of the explicit functions
     let q = mat!([0.0f64, 2.0f64, 0.0f64, 0.0f64]);
@@ -620,5 +647,12 @@ fn main() {
     let c = wedge_m(A.as_ref(), B.as_ref());
     
     println!("{:?}", c);
-   
+    /* 
+    let c: Mat<f64> = wedge(p.as_ref(), q.as_ref()); //c is a 2-form
+    let k: Mat<f64> = wedge(r.as_ref(), c.as_ref()); //k should be a 3-form as wedge product is associative
+    let K: Mat<f64> = wedge(s.as_ref(), k.as_ref()); //should be a 4-form
+    println!("{:?}", c);
+    println!("{:?}", k);
+    println!("{:?}", K);
+    */
 }
